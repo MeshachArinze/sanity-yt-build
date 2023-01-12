f8755e2d7c87eeff9a341560bf0713ae6b8c43de
MeshachArinze <meshacharinze@gmail.com>
2023-01-12 17:41:30 +0100

    only commit new changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,15 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex justify-between p-5">
+    <header className="flex justify-between p-5 max-w-7xl">
       <div className="flex items-center space-x-5">
         <Link href="/">
             <img className="w-44 object-contain cursor-pointer" src="https://links.paparect.com/yvf" alt="" />
         </Link>
-        <div className="hidden md:inline-flex">
+        <div className="hidden md:inline-flex items-center  space-x-5">
             <h3>About</h3>
             <h3>Contact</h3>
-            <h3 className="bg-green text-white py-1 rounded-full">Follow</h3>
+            <h3 className=" bg-green-600 text-white rounded-full px-4 py-1 ">Follow</h3>
         </div>
       </div>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,16 +12,16 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <div className='flex justify-between items-center bg-yellow-400 border-black border-y py-10 lg:py-0'>
-        <div className='pt-10 space-y-5'>
+        <div className='px-10 space-y-5'>
           <h1 className='text-6xl max-w-xl font-serif'>
             <span className='underline decoration-black decoration-4'>
               Medium
-            </span>
-            is a place to write, read and correct
+            </span>{" "}
+             is a place to write, read and correct
           </h1>
           <h2>Its easy and free to post your thinking on any topic and correct millions readers</h2>
         </div>
-        <img className='hidden md:inline-flex h032 lg-h-full' src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
+        <img className='hidden md:inline-flex h-32 lg:h-full' src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
       </div>
     </div>
   )
